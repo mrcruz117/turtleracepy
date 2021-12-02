d0e17fed478b6bbf285281480a44f3b70342a1e9
mrcruz117 <mrcruz1989@gmail.com>
2021-12-02 16:22:23 -0500

    made turtle generator

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 from turtle import Turtle, Screen
 
 screen = Screen()
-tim = Turtle()
+screen.setup(width=500, height=400)
+user_bet = screen.textinput(title='Make your bet', prompt="Which turtle will win the race? Choose a color: ")
+colors = ['red', 'orange', 'black', 'green', 'blue', 'purple']
 
+starting_y = -80
 
+for turtle_idx in range(len(colors)):
+    tim = Turtle(shape='turtle')
+    tim.color(colors[turtle_idx])
+    tim.penup()
+    tim.goto(x=-240, y=starting_y)
+    starting_y += 30
 
 screen.exitonclick()
